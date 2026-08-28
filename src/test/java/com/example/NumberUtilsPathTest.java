2a416d36586eb9ce9af26ce71a4c9f0f6c7e04f5
hoangtrungtuyen123 <153747110+hoangtrungtuyen123@users.noreply.github.com>
2026-08-28 08:25:51 +0700

    fix: correct syntax errors in path tests

--- src/test/java/com/example/NumberUtilsPathTest.java
+++ src/test/java/com/example/NumberUtilsPathTest.java
@@ -19,19 +19,16 @@ public class NumberUtilsPathTest {
 
     @Test
     void testPathNegativeAndZero() {
-        // Kiem tra nhanh num <= 0
         assertEquals(0, utils.countPositiveEvens(new int[]{-4, -2, 0}));
     }
 
     @Test
     void testPathPositiveOdd() {
-        // Kiem tra nhanh num > 0 nhung num % 2 != 0
         assertEquals(0, utils.countPositiveEvens(new int[]{1, 3, 5}));
     }
 
     @Test
     void testPathPositiveEvenAndMixed() {
-        // Kiem tra nhanh num > 0 va num % 2 == 0 ket hop
         assertEquals(2, utils.countPositiveEvens(new int[]{-1, 3, 4, 6}));
     }
 }
